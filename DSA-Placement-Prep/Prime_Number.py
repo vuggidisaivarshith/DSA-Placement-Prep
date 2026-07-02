b218d2b90cc58cce6cd0270315a6f94bb82adf18
vuggidisaivarshith <vuggidisaivarshith@gmail.com>
2026-07-02 21:53:41 +0530

    Add local folder files

---
+++ DSA-Placement-Prep/Prime_Number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPrime(self, n):
+        if n <= 1:
+            return False
+
+        for i in range(2,int(n**0.5)+1):
+            if n%i==0:
+                return False
+
+        return True
